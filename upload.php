94832793eb21ae1390e9e6e5745d4b606beca033
angie_ad <35981847+aguilarzand@users.noreply.github.com>
2022-05-03 13:31:50 -0500

    Update upload.php

--- upload.php
+++ upload.php
@@ -8,11 +8,6 @@ echo "nombre tmp: ".$filetmpname;
 echo "tipo: ".$filetype;
 echo "tamaño: ".$filesize;
 $imagen = "img/".$filename;
-if($filetype == "image/jpeg" && $filesize <=2000000){
-	move_uploaded_file($filetmpname,$imagen);
-	echo "<img src='$imagen' width=80% height=80% />";
-}
-else{
-	echo "la imagen no cumple con lo solicitado";
-}
+move_uploaded_file($filetmpname,$imagen);
+echo "<img src='$imagen' width=80% height=80% />";
 ?>
